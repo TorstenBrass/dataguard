575ee687d1147d3fbdf9df0fc1b32dca9101b689
TorstenBrass <torstenbrass@gmail.com>
2026-05-19 18:03:47 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DataGuardApp from './DataGuard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DataGuardApp />
+  </React.StrictMode>
+)
